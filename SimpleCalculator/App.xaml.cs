35cc52974d363bc738c0125f881752d4e034a6e7
Hashim Atiq <hashimqh@gmail.com>
2023-06-27 13:43:23 +0500

    Windows App Screenshot and Window Size Configuration.

--- SimpleCalculator/App.xaml.cs
+++ SimpleCalculator/App.xaml.cs
@@ -8,4 +8,17 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        const int newWidth = 400;
+        const int newHeight = 750;
+
+        window.Width = newWidth;
+        window.Height = newHeight;
+
+        return window;
+    }
 }
