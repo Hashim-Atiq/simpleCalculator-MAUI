428393d7c9941178b0348aaa352f8171ef4f91af
Hashim Atiq <hashimqh@gmail.com>
2023-06-27 12:27:09 +0500

    Add project files.

---
+++ SimpleCalculator/Calculate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public static class Calculate
+    {
+        public static double CalculateResult(double num1, double num2, char operation)
+        {
+            switch (operation)
+            {
+                case '+':
+                    return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                case '*':
+                    return num1 * num2;
+                case '/':
+                    return num1 / num2;
+                default:
+                    return -1;
+            }
+        }
+    }
+}
---
+++ SimpleCalculator/MainPage.xaml.cs
@@ -0,0 +1,100 @@
+﻿namespace SimpleCalculator;
+
+public partial class MainPage : ContentPage
+{
+    string number = "";
+    double firstNumber = 0;
+    double secondNumber = 0;
+    double result = 0;
+    int state = 1;
+    char operation = '\0';
+
+    public MainPage()
+	{
+		InitializeComponent();
+        this.ResultField.Text = "0";
+    }
+
+    void OnNumberClick(Object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+
+        number += button.Text;
+        if (number.All(res => res == '0'))
+        {
+            number = "";
+            this.ResultField.Text = "0";
+        }
+        else
+        {
+            this.ResultField.Text = number;
+        }
+    }
+
+    void OnClearResultField(Object sender, EventArgs e)
+    {
+        number = "";
+        this.ResultField.Text = "0";
+        state = 1;
+    }
+
+    void OnActionClick(Object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+
+        if (state == 1)
+        {
+            firstNumber = double.Parse(this.ResultField.Text);
+            operation = button.Text[0];
+            number = "";
+            state++;
+        }
+        else if (state > 1)
+        {
+            secondNumber = double.Parse(this.ResultField.Text);
+            result = CalculateResult(firstNumber, secondNumber, operation);
+            this.ResultField.Text = result.ToString();
+            firstNumber = result;
+            operation = button.Text[0];
+            secondNumber = 0;
+            number = "";
+            state++;
+        }
+    }
+
+    void OnResultClick(Object sender, EventArgs e)
+    {
+        secondNumber = double.Parse(this.ResultField.Text);
+        result = CalculateResult(firstNumber, secondNumber, operation);
+        this.ResultField.Text = result.ToString();
+        firstNumber = result;
+        firstNumber = 0;
+        secondNumber = 0;
+        number = "";
+        state = 1;
+    }
+
+    double CalculateResult(double firstNumber, double secondNumber, char operation)
+    {
+        return Calculate.CalculateResult(firstNumber, secondNumber, operation);
+    }
+
+    void getNegative(Object sender, EventArgs e)
+    {
+        if (this.ResultField.Text != "0")
+        {
+            this.ResultField.Text = (double.Parse(this.ResultField.Text) * -1).ToString();
+        }
+    }
+
+    void getPercent(Object sender, EventArgs e)
+    {
+        this.ResultField.Text = (double.Parse(this.ResultField.Text) / 100).ToString();
+    }
+
+    void showError()
+    {
+        this.ResultField.Text = "ERR";
+    }
+}
+
